b38740244d298f1ce7075a3c3de1430d104aa215
awj90 <dennis.awj@gmail.com>
2023-02-13 12:07:24 +0800

    updated with client code

--- src/myapp/client/ClientApp.java
+++ src/myapp/client/ClientApp.java
@@ -1,10 +1,63 @@
 package myapp.client;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class ClientApp {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
+        Socket socket = new Socket("localhost", 1234);
         
+        InputStream is = socket.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(is);
+        DataInputStream dis = new DataInputStream(bis);
 
+        OutputStream os = socket.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        Console console = System.console();
+        String responseToServer = "";
+        String responseFromServer = "";
+
+        while (true) {
+            printMenu();
+            responseToServer = console.readLine();
+            dos.writeUTF(responseToServer);
+            dos.flush();
+            if (responseToServer.equalsIgnoreCase("quit")) {
+                System.out.println("Ending program... Goodbye!");
+                break;
+            }
+            responseFromServer = dis.readUTF();
+            System.out.println(responseFromServer);
+            if (responseToServer.contains("correct")) {
+                break;
+            }
+        }
+
+        bos.flush();
+        os.flush();
+        dos.close();
+        bos.close();
+        os.close();
+        dis.close();
+        bis.close();
+        is.close();
+        socket.close();
+    }
+
+    public static void printMenu() {
+        System.out.println("""
+            Enter "quit" to quit the program, or
+            Enter "guess X" to guess a number X between 0 to 99: """);
     }
 }
--- src/myapp/server/App.java
+++ src/myapp/server/App.java
@@ -1,16 +1,25 @@
 package myapp.server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         Random random = new Random(); // instantiate a Random object
         
         // instantiate a Scanner object
-        Scanner scanner = new Scanner(System.in); // Scanner(System.in) scans from console input, Scanner(File object) scans from file, Scanner(InputStream object) scans from InputStream
+        // Scanner scanner = new Scanner(System.in); // Scanner(System.in) scans from console input, Scanner(File object) scans from file, Scanner(InputStream object) scans from InputStream
         // eg. for client-server, Scanner(socket.getInputStream())
         
         int randomInteger = random.nextInt(100); // generates a random integer between 0 - 99
@@ -18,21 +27,50 @@ public class App {
         // initialize a guess
         int guess = 0;
 
-        // allow user to guess until they get the random number correctly
-        while (guess != randomInteger) {
-            System.out.println("Please guess a number: ");
-            guess = scanner.nextInt();
+        // Instantiate a ServerSocket and Socket object listening on port 1234 for input
+        ServerSocket server = new ServerSocket(1234);
+        System.out.println("Server started...");
+        Socket socket = server.accept();
+
+        InputStream is = socket.getInputStream();
+        BufferedInputStream bis = new BufferedInputStream(is);
+        DataInputStream dis = new DataInputStream(bis);
+
+        OutputStream os = socket.getOutputStream();
+        BufferedOutputStream bos = new BufferedOutputStream(os);
+        DataOutputStream dos = new DataOutputStream(bos);
+
+        String messageReceived = "";
+
+        while (!messageReceived.equalsIgnoreCase("quit")) {
+        
+            messageReceived = dis.readUTF(); // user inputs: "guess X", where X is an integer 
+            
+            if (messageReceived.contains("guess")) {
+                guess = Integer.parseInt(messageReceived.substring(6));
+            }
 
             if (guess == randomInteger) {
-                System.out.println("You guessed the correct number!");
+                dos.writeUTF("You guessed the correct number!");
+                dos.flush();
                 break;
             } else if (guess > randomInteger) {
-                System.out.println("The number you guessed is too high.");
+                dos.writeUTF("The number you guessed is too high.");
+                dos.flush();
             } else if (guess < randomInteger) {
-                System.out.println("The number you guessed is too low.");
+                dos.writeUTF("The number you guessed is too low.");
+                dos.flush();
             }
         }
-        scanner.close();
 
+        bos.flush();
+        os.flush();
+        dos.close();
+        bos.close();
+        os.close();
+        dis.close();
+        bis.close();
+        is.close();
+        server.close();
     }
 }
